5793684e57a770df5f3b0009b5c02afe71bec636
ginalamp <gina.n.lamprecht@gmail.com>
2021-10-08 18:05:30 +0200

    Add module constructor

--- oop/degree.go
+++ oop/degree.go
@@ -5,12 +5,26 @@ type Degree struct {
 	Modules []Module
 }
 
+// assumption: all modules weigh the same amount to the degree
 type Module struct {
-	Name       string
-	Weight     int // weight of the module in the total degree
+	Name string
+	// Weight     int // weight of the module in the total degree
 	Mark       int
 	Components []Component
 }
+
+// init module
+func NewModule(moduleName string) Module {
+	module := Module{}
+	module.Name = moduleName
+	module.Mark = 0
+	return module
+}
+
+// func addModuleComponent(componentName string, mark, weight int) Component {
+// 	component := Component{}
+// 	component.Name := componentName
+// }
 
 // e.g. assighment
 type Component struct {
