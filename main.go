23e17b423b7781c823f920684e1c15afea2d9983
ginalamp <gina.n.lamprecht@gmail.com>
2021-10-08 18:07:43 +0200

    Read module csv & create module basic name struct

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio" // user terminal input
+	"encoding/csv"
 	"fmt"
+	"log"
 
 	"ginalamp-mark-tracker/oop"
 	"os"      // user input
@@ -16,24 +18,61 @@ func readInput(userPrompt string) string {
 	userInput, _ := reader.ReadString('\n')
 	userInput = strings.TrimSpace(userInput) // remove whitespace
 
-	fmt.Println("input read - ", userInput)
+	// fmt.Println("input read: ", userInput)
 	return userInput
 }
 
-func main() {
-	// m := degree.Module{}
-	// fmt.Println(m)
-
-	numModules := 0
-	for numModules < 1 {
-		numModules = stringToInt(readInput("How many modules do you have?"))
+// read csv file
+// https://stackoverflow.com/questions/24999079/reading-csv-file-in-go
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
 	}
-	fmt.Println(numModules)
+	defer f.Close()
 
-	// degree.Module
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
+}
+
+func main() {
+	fmt.Println("Welcome to Gina's Mark Calculator")
+	inputType := stringToInt(readInput("Enter 0 to import a csv, Enter 1 to manually add entries:"))
+
+	if inputType == 0 {
+		records := readCsvFile("temp.csv")
+		// numModules := len(records) - 1
+		// fmt.Println(len(records))
+		for i, row := range records {
+			// fmt.Println(row)
+			// skip title/1st row
+			if i == 0 {
+				continue
+			}
+			moduleData := strings.Split(row[0], ";")
+			// fmt.Println(moduleData[0])
+			fmt.Println(oop.NewModule(moduleData[0]))
+		}
+		// for i := 0; i < numModules; i++ {
+
+		// 	// oop.Component{Name: "Accounting", Mark: 77, Weight: 50}
+		// }
+	} else if inputType == 1 {
+		// read input from terminal
+		numModules := 0
+		for numModules < 1 {
+			numModules = stringToInt(readInput("How many modules do you have?"))
+		}
+		fmt.Println(numModules)
+	}
 
 	// basic assignment initialisation
-	a2 := oop.Assignment{AssignmentName: "Accounting", Mark: 77, Weight: 50}
+	a2 := oop.Component{Name: "Accounting", Mark: 77, Weight: 50}
 	fmt.Println(a2)
 }
 
@@ -41,7 +80,7 @@ func main() {
 func stringToInt(s string) int {
 	i, err := strconv.Atoi(s)
 	if err != nil {
-		fmt.Println("Invalid input. Please type in the number of modules that you have (as an integer)")
+		fmt.Println("Invalid input. Please type in a number (integer)")
 		// handle error
 		fmt.Println(err)
 		return (0)
